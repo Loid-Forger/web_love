60a173820cbc56f4f649f17bdba3b799be80f76e
Loiger <you@example.com>
2021-12-31 22:28:15 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 He nhô ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Mai 2021 rồi có phải em thấy anh rất đẹp trai khôngg ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, em thấy anh rất đẹp trai! 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao em lại nghĩ như vậy 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: ' tại vì anh rất giống với người yêu tương lai của em😊😊',
+    mess: 'Anh biết mà, Anh cũng thích Ăn kem  lắm 🥰. ',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/messages/t/100024500268707'
+}
